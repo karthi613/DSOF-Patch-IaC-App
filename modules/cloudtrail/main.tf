54e2181e8249683558fc7ee03ed1d4a38826e03f
karthikeyan_m <karthikeyan.m@idp.com>
2024-10-24 12:28:07 +0530

    snyk

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -2,6 +2,15 @@ resource "aws_cloudtrail" "insecure-logging" {
   name           = "cloudtrail-logging"
   s3_bucket_name = "my-cloudtrail-bucket"
   enable_logging = true
-enable_log_file_validation  = true
-is_multi_region_trail = true
+  enable_log_file_validation  = true
+  is_multi_region_trail = true
+  cloud_watch_logs_group_arn = aws_cloudwatch_log_group.insecure-logging.arn
+}
+
+resource "aws_cloudwatch_log_group" "insecure-logging" {
+  name = "aws_cloudtrail_log"
+  retention_in_days = 1
+  tags = {
+    Name = "aws_cloudtrail_log"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,12 @@ variable "region" {
   type    = string
   default = "eu-west-1"
 }
+
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
